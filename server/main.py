c82264984e6630ffbee4e9503d7c848e49142ace
Nick Pearson <nicholas.pearson4@btinternet.com>
2020-04-30 23:55:14 +0100

    Set bind url

--- server/main.py
+++ server/main.py
@@ -112,4 +112,4 @@ if __name__ == "__main__":
     print("Starting server");
 
     app.testing = settings["debug"];
-    app.run(debug=settings["debug"], port=settings["port"]);
+    app.run(host='0.0.0.0', debug=settings["debug"], port=settings["port"]);
