575fe807f1a31428fa18e990c54e25b20fbe0750
Nick Pearson <nicholas.pearson4@btinternet.com>
2020-05-02 11:07:36 +0100

    More debugging

--- custom-entrypoint.sh
+++ custom-entrypoint.sh
@@ -1,7 +1,11 @@
 #! /usr/bin/env sh
 export LISTEN_PORT=${PORT:-80}
 echo "Server starting on port ${LISTEN_PORT}"
+pwd
+echo "ls"
 ls
+echo "ls /"
+ls /
 echo "Running:"
 cat '/main-entrypoint.sh'
 exec '/main-entrypoint.sh'
