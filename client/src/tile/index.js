1a5fa765bb8db5c18efb36023e01016fed84f37c
Nick Pearson <nicholas.pearson4@btinternet.com>
2020-05-10 15:19:22 +0100

    Swtich up clearing and base tiles

--- client/src/tile/index.js
+++ client/src/tile/index.js
@@ -1,12 +1,11 @@
-import Base from "./sprites/base.png";
+import Base0 from "./sprites/base0.png";
+import Base1 from "./sprites/base1.png";
 
 import Forest0 from "./sprites/forest0.png";
 import Forest1 from "./sprites/forest1.png";
 
 import Clearing0 from "./sprites/clearing0.png";
 import Clearing1 from "./sprites/clearing1.png";
-import Clearing2 from "./sprites/clearing2.png";
-
 import Mountains0 from "./sprites/mountain0.png";
 
 import TownHall from "./sprites/townhall.png";
@@ -17,9 +16,9 @@ import Sawmill from "./sprites/sawmill.png";
 
 const tileFilepaths =
 {
-  base: [Base],
+  base: [Base0, Base1],
   forest: [Forest0, Forest1],
-  clearing: [Clearing0, Clearing1, Clearing2],
+  clearing: [Base0, Base1, Clearing0, Clearing1],
   mountains: [Mountains0],
 };
 
